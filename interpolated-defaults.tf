660085ad492d19066fad2aad96fa6fb2c6185b70
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-08 10:40:06 +0100

    feat: added networking resources

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -4,4 +4,22 @@ data "azurerm_subscription" "current" {
 locals {
   name    = var.name != "" ? var.name : var.project
   is_prod = length(regexall(".*(prod).*", var.env)) > 0
+  subscription_vnet_map = {
+    "ae75b9fb-7d34-4112-82ff-64bd3855ce27" = {
+      vnet_name           = "vnet-nle-int-01"
+      vnet_resource_group = "InternalSpoke-rg"
+    }
+    "9c604868-4643-43b8-9eb1-4c348c739a3e" = {
+      vnet_name           = "vnet-nle-ext-01"
+      vnet_resource_group = "ExternalSpoke-rg"
+    }
+    "17390ec1-5a5e-4a20-afb3-38d8d726ae45" = {
+      vnet_name           = "vnet-prod-int-01"
+      vnet_resource_group = "InternalSpoke-rg"
+    }
+    "c170c61a-f00f-4295-a718-634e27af1597" = {
+      vnet_name           = "vnet-prod-ext-01"
+      vnet_resource_group = "ExternalSpoke-rg"
+    }
+  }
 }
