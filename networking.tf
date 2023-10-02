289d93c011d9a8162824c64c4e4d1006769af95b
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-02 11:08:28 +0100

    fix: specify existing rg

--- networking.tf
+++ networking.tf
@@ -1,11 +1,12 @@
 module "networking" {
   source = "github.com/hmcts/terraform-module-azure-virtual-networking?ref=main"
 
-  env         = var.env
-  product     = var.project
-  common_tags = var.common_tags
-  component   = "networking"
-  name        = local.name
+  env                          = var.env
+  product                      = var.project
+  common_tags                  = var.common_tags
+  component                    = "networking"
+  name                         = local.name
+  existing_resource_group_name = local.subscription_vnet_map[data.azurerm_subscription.current.subscription_id].vnet_resource_group
 
   vnets = {
     "${local.subscription_vnet_map[data.azurerm_subscription.current.subscription_id].vnet_name}" = {
